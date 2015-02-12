412590caec1043492c02042cb64232544794c652
Misael Taveras <macrot29@gmail.com>
2015-02-12 19:45:59 -0400

    El mas grande de Todos :3 :dart:
    Hemos agregado un Footer, con la descripción, hemos hecho que los
    programas se Descargue :smiley: y se incremente el Campo de "Rating" de
    cada programa, emerge una "TOAST" diciendo que el programa se ha
    descargado.

--- public/js/appControllers.js
+++ public/js/appControllers.js
@@ -7,21 +7,46 @@ function HomeCtrl($scope, $http, $timeout) {
     // when landing on the page, get all programs  and show them
     $http.get('/api/programas')
         .success(function(data) {
-        $scope.programas = [];
+          if (data.length<1) {$scope.message = {error:'Nada encontrado'};}
+          $scope.programas = [];
           $timeout(function () {
             for (var count = 0; count < data.length; count++) {
-              $scope.programas.push(data[count]);
+            $scope.programas.push(data[count]);
             }
           }).then(function(){
-                 $timeout(function(){
-                    new WOW().init({offset: 500});
+            $timeout(function(){
+            new WOW().init({offset: 1500});
           });
-            console.log(data);
-        });
+          });
       })
         .error(function(data) {
             console.log('Error: ' + data);
         });
+
+    $scope.upRatting = function(programId) {
+      var URI = '/api/programas/'+programId,
+            update = {query:{$inc:{rating:1}}};
+      $http.post(URI, update)
+      .success(function(data){
+          $scope.programas = [];
+
+          $timeout(function () {
+            for (var count = 0; count < data.length; count++) {
+            $scope.programas.push(data[count]);
+            }
+          }).then(function(){
+            $timeout(function(){
+            new WOW().init({offset: 1500});
+          });
+          });
+          toast('Archivo Descargado', 4000);
+        }
+        )
+        .error(function(error){
+                  toast('Error :(  <a href="#"  class="red darken-2 white-text" ng-click="getDownload('+ programId +')">Reintentar</a>', 4000);
+                }
+          );
+    };
 }
 function CategoryCtrl($scope, $http) {
     // when landing on the page, get just a kind of Program and show them
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,29 @@ String.prototype.capitalize = function(){
 
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 var Programa = require('../models/programs.js');
+
+
+/*router.get('/api/masive', function(req, res){
+  Programa.create({
+    'nombre' : 'Algun Programa',
+    'resumen' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat maiores dolore voluptatem.',
+    'url' : '\\programs\\1.txt',
+    'descripcion' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore harum quasi numquam eaque blanditiis ex earum aliquid, accusantium placeat iste recusandae laboriosam voluptatibus culpa, quas!',
+    'rating' : 0,
+    'tipo' : 'IDE',
+    'fecha' : 'Mon Feb 09 2015 19:35:56 GMT-0400 (Hora estándar oeste, Sudamérica)'
+  }, function(err, done){
+    if (err) {console.log(err);}
+    console.log(done);
+    res.redirect('/');
+  });
+});*/
+
+
+
 
 // GET home page.
 router.get('/api/programas', function(req, res) {
@@ -30,20 +51,35 @@ router.get('/api/programa/:programaId', function(req, res){
 router.delete('/api/programa/:programaId', function(req, res){
   Programa.remove({_id: req.params.programaId}, function(err, programa){
     if(err){res.send(err); console.log(err);}
+    console.log(programa);
     res.json({message: 'Borrado Satisfactoriamente'});
   });
 });
 
-router.put('/api/programas/:programaId', function(req, res){
+router.post('/api/programas/:programaId', function(req, res){
+  Programa.findByIdAndUpdate(req.params.programaId, req.body.query, function(err, programa){
+    if (err) {console.log(err);}
+    Programa.find(function(err, programas) {
+        if(err){
+          res.send(err);
+          console.log(err);
+        }
+        res.json(programas);
+  });
+  });
+});
+
+router.get('/api/programas/download/:programaId', function(req, res){
   Programa.findById(req.params.programaId, function(err, programa){
-    if(err){res.send(err); console.log('Erro en find: ' + err);}
-    programa.nombre = req.body.nombre;
-    programa.resumen = req.body.resumen;
-    programa.url = req.body.url;
-    programa.descripcion = req.body.descripcion;
-    programa.fecha = req.body.fecha;
-    programa.rating = req.body.rating;
-    programa.tipo = req.body.tipo;
+    if (err) {res.json(err);console.log('Programa no encontrado: '+ err); return;}
+    var file = path.join(__dirname, '../public', programa.url);
+    console.log(file);
+    res.download(file, path.basename(file), function(err){
+      if (err) {console.log('Error en Descarga: ' + err); res.json(err);}
+      else{
+        console.log('success');
+      }
+    });
   });
 });
 
